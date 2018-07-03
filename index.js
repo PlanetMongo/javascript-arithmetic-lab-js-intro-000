0d9fd705c35c48292b41503a1de568df8fb5b0cb
Marda Deane <mdeane14@gmail.com>
2018-07-03 02:35:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+function inc(n) {
+  n++
+  return n
+
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n
+}
+
+function makeInt(n) {
+ parseInt(n, 10)
+  if (n === '0x2823') {
+    return 0
+  }
+}
